a3a480434627e8b586ec31a6a22cff320f429a53
lazyboy007 <sami.bosch@gmail.com>
2019-09-16 16:41:39 +0200

    Basic setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='forester',
+    packages=['forester'],
+    include_package_data=True,
+    install_requires=[
+        'numpy', 'scikit-learn'
+    ],
+)
