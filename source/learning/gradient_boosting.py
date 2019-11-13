09faa15576ae57d5aab8ed1738fb7ba23273686b
lazyboy007 <sami.bosch@gmail.com>
2019-11-13 15:25:57 +0100

    add gradient boosting

--- source/learning/gradient_boosting.py
+++ source/learning/gradient_boosting.py
@@ -1,3 +1,4 @@
+from sklearn.ensemble import GradientBoostingClassifier
 from source.utils import file_manager
 from source.learning import learning
 
@@ -5,12 +6,13 @@ from source.learning import learning
 class GradientBoosting(learning.Learning):
     def __init__(self, data_set: file_manager.DataSet, b=False, **kwargs):
         super().__init__(data_set, b=b)
+        self.g = GradientBoostingClassifier(kwargs)
 
     def build(self):
-        pass
+        self.g.fit(self.data_set.data, self.data_set.classes)
 
     def run(self, data_set: file_manager.DataSet):
-        pass
+        self.g.predict(data_set.data)
 
     def write_to_file(self):
         pass
