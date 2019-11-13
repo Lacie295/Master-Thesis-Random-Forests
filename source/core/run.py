d25bf1e46ada6f17d11e7318f77c9b044bd11be2
lazyboy007 <sami.bosch@gmail.com>
2019-11-13 14:10:41 +0100

    Add dataset object for cleanliness

--- source/core/run.py
+++ source/core/run.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     parser.add_argument('-m', '--method', nargs="+")
     parser.add_argument('-i', '--input', nargs="+")
     parser.add_argument('-g', '--graph', action="store_true")
-    parser.add_argument('-a', '--graph', action="store_true")
+    parser.add_argument('-a', '--all_methods', action="store_true")
 
     a = parser.parse_args()
     main(a)
--- source/utils/file_manager.py
+++ source/utils/file_manager.py
@@ -3,6 +3,23 @@ import re
 data_sets = {}
 
 
+class DataSet:
+    file = ""
+    classes = {}
+    labels = {}
+    data = []
+    converted_data = []
+
+    def __init__(self, file):
+        self.file = file
+
+    def convert_data(self):
+        self.converted_data = [[0] * len(self.labels)] * len(self.data)
+        for i in range(len(self.data)):
+            for j in self.data[i]:
+                self.converted_data[i][j] = 1
+
+
 def read(files):
     for f in files:
         file = open(f)
@@ -11,38 +28,55 @@ def read(files):
 
 def parse(file):
     lines = file.readlines()
-    data = []
-    labels = {}
-    classes = {}
+    d = DataSet(file.name)
     reading_data = False
     for line in lines:
-        if line:
+        if line.strip():
             tokens = line.strip().split(" ")
             if re.match(r"^@[0-9]+:$", tokens[0]) and not reading_data:
                 n = int(tokens[0][1:-1])
                 label = line[len(tokens[0]):].strip()
-                labels[n] = label
+                d.labels[n] = label
             elif re.match(r"^@class:$", tokens[0]) and not reading_data:
                 for token in tokens[1:]:
                     t = token.split("=")
                     n = int(t[0])
                     c = t[1]
                     c = c[2:-2] if re.match(r"'{.*}'", c) else c[1:-1]
-                    classes[n] = c
-            elif re.match(r"^@data:$", tokens[0]) and not reading_data:
+                    d.classes[n] = c
+            elif re.match(r"^@data$", tokens[0]) and not reading_data:
                 reading_data = True
             elif re.match(r"^@.*$", tokens[0]):
                 pass
             elif re.match(r"^[0-9]+$", tokens[0]) and reading_data:
-                d = []
+                data = []
                 for token in tokens:
                     i = int(token)
-                    d.append(i)
-                data.append(d)
+                    data.append(i)
+                d.data.append(data)
             else:
                 raise RuntimeError("Incorrect formatting!")
-    return classes, labels, data
+    return d
 
 
 def get_file(file):
     return data_sets[file]
+
+
+def get_data(file):
+    return get_file(file).data
+
+
+def get_classes(file):
+    return get_file(file).classes
+
+
+def get_labels(file):
+    return get_file(file).labels
+
+
+def get_converted(file):
+    d = get_file(file)
+    if not d.converted_data:
+        d.convert_data()
+    return d.converted_data
